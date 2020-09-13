6c8df434058efc73b45f64d271ea155462530c60
mateus4k <mateus4k@protonmail.ch>
2020-09-13 10:21:22 -0300

    feat(exceptions): add layer

---
+++ src/exceptions/index.ts
@@ -0,0 +1,4 @@
+export { default as MissingParamError } from './missing-param-error';
+export { default as InvalidParamError } from './invalid-param-error';
+export { default as ServerError } from './server-error';
+export { default as UnauthorizedError } from './unauthorized-error';
