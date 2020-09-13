d816ef952fb872c54fad3208ce3a2188574ac30a
mateus4k <mateus4k@protonmail.ch>
2020-09-13 10:21:42 -0300

    feat(helpers): add http-response helpers

---
+++ src/helpers/http/http-response.ts
@@ -0,0 +1,46 @@
+import { UnauthorizedError, ServerError } from '../../exceptions';
+
+interface IResponse {
+  statusCode: number;
+  body: {
+    error?: string;
+  };
+}
+
+class HttpResponse {
+  static ok(body: unknown): IResponse {
+    return {
+      statusCode: 200,
+      body,
+    };
+  }
+
+  static badRequest(error: Error): IResponse {
+    return {
+      statusCode: 400,
+      body: {
+        error: error.message,
+      },
+    };
+  }
+
+  static unauthorizedError(): IResponse {
+    return {
+      statusCode: 401,
+      body: {
+        error: new UnauthorizedError().message,
+      },
+    };
+  }
+
+  static serverError(): IResponse {
+    return {
+      statusCode: 500,
+      body: {
+        error: new ServerError().message,
+      },
+    };
+  }
+}
+
+export default HttpResponse;
