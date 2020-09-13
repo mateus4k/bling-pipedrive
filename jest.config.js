f621f26caea970d8032d8991c4ad5dbaa85b75ef
mateus4k <mateus4k@protonmail.ch>
2020-09-13 10:21:08 -0300

    fix(lint): general

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.d.ts'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
-};
+  coveragePathIgnorePatterns: ['/node_modules/']
+}
