d08dff2d86cdfe797ec864641fb44a7d31d6be46
mateus4k <mateus4k@protonmail.ch>
2020-09-14 11:11:01 -0300

    chore(lint): remove explicit-module-boundary-types rule

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -3,7 +3,6 @@ import { ControllerInterface } from '../../controllers/controller-interface';
 
 export class ExpressRouterAdapter {
   static adapt(router: ControllerInterface) {
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     return async (req: Request, res: Response) => {
       const httpRequest = {
         body: req.body,
