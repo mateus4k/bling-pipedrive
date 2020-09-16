b61687be4afc5d071c57ebf936ff7649f26ccfc9
mateus4k <mateus4k@protonmail.ch>
2020-09-16 14:42:47 -0300

    feat(main): connect to mongo before start

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,10 @@
 import { app, env } from './config';
+import { MongoHelper } from '../infra/helpers';
 
-app.listen(env.port, () => {
-  console.log(`🚀 App is running at http://localhost:${env.port}`);
-});
+MongoHelper.connect(env.mongo.url)
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(`🚀 App is running at http://localhost:${env.port}`);
+    });
+  })
+  .catch(console.error);
