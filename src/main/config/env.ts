1487ee102dc721d9eaa9f30b0cd716857f05d0d3
mateus4k <mateus4k@protonmail.ch>
2020-09-15 21:41:49 -0300

    chore(env): rename url to baseUrl

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -6,11 +6,11 @@ export const env = {
   port: process.env.PORT || 3333,
   mongo: process.env.MONGO_URL || '',
   pipedrive: {
-    url: 'https://developers.pipedrive.com/docs/api/v1/',
+    baseUrl: 'https://developers.pipedrive.com/docs/api/v1/',
     token: process.env.PIPEDRIVE_TOKEN || '',
   },
   bling: {
-    url: 'https://bling.com.br/Api/v2/pedidos/json/',
+    baseUrl: 'https://bling.com.br/Api/v2/pedidos/json/',
     token: process.env.BLING_TOKEN || '',
   },
 };
