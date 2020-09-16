dd204fb5b8c2d9c7f10b607a03c66cd8203db18e
mateus4k <mateus4k@protonmail.ch>
2020-09-15 21:19:11 -0300

    fix(env): get variables from env with dotenv package

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,10 @@
+import { config } from 'dotenv';
+
+config();
+
 export const env = {
-  port: process.env.PORT ?? 3333,
-  mongo: process.env.MONGO_URL ?? '',
-  pipedrive: process.env.PIPEDRIVE_TOKEN ?? '',
-  bling: process.env.BLING_TOKEN ?? '',
+  port: process.env.PORT || 3333,
+  mongo: process.env.MONGO_URL || '',
+  pipedrive: process.env.PIPEDRIVE_TOKEN || '',
+  bling: process.env.BLING_TOKEN || '',
 };
