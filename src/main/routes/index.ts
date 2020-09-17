7511ab89c57ca9681eb12fb9fb0b90f9bf72850f
Mateus <matheus.sampaio@bee.com.br>
2020-09-16 21:52:48 -0300

    feat(deals): add composer to routes

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { ExpressRouterAdapter } from '../adapters/express-router';
-import { StoreOrdersComposer } from '../composers';
+import { IndexOrdersComposer, StoreOrdersComposer } from '../composers';
 
 const adapter = ExpressRouterAdapter.adapt;
 const router = Router();
 
-// router.get('/deals', adapter(StoreOrdersComposer.compose()));
+router.get('/deals', adapter(IndexOrdersComposer.compose()));
 router.post('/deals', adapter(StoreOrdersComposer.compose()));
 
 export { router };
