40326b413b145524c2cbbb2dce9107b81358c688
Mateus <matheus.sampaio@bee.com.br>
2020-09-16 21:16:39 -0300

    feat(deals): add all and find methods

--- src/infra/repositories/deals-repository.ts
+++ src/infra/repositories/deals-repository.ts
@@ -3,6 +3,18 @@ import { MongoHelper } from '../helpers';
 export class DealsRepository {
   private collection = 'deals';
 
+  async all() {
+    const model = await MongoHelper.getCollection(this.collection);
+    const response = await model.find();
+    return response;
+  }
+
+  async find(id: unknown) {
+    const model = await MongoHelper.getCollection(this.collection);
+    const response = await model.findOne(id);
+    return response;
+  }
+
   async insert(data: unknown) {
     const model = await MongoHelper.getCollection(this.collection);
     const response = await model.insertOne(data);
