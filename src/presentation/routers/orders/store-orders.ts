b15f6a853e50b903996d8e82e2b16b851fff8cdb
Mateus <matheus.sampaio@bee.com.br>
2020-09-16 21:14:18 -0300

    feat(routers): store orders router

---
+++ src/presentation/routers/orders/store-orders.ts
@@ -0,0 +1,31 @@
+import { UseCaseInterface } from '../../../domain/usecases/interface';
+import { RouterInterface } from '../interface';
+import { HttpResponse, ResponseInterface } from '../../helpers';
+
+export class StoreOrdersRouter implements RouterInterface {
+  constructor(
+    private getDealsPipedriveCase: UseCaseInterface,
+    private createOrdersBlingCase: UseCaseInterface,
+    private saveManyDealsUseCase: UseCaseInterface,
+  ) {}
+
+  async route(): Promise<ResponseInterface> {
+    try {
+      const deals = await this.getDealsPipedriveCase.run();
+
+      if (!Array.isArray(deals)) {
+        return HttpResponse.notFoundError();
+      }
+
+      const createOrders = () => this.createOrdersBlingCase.run(deals);
+      const saveManyDeals = () => this.saveManyDealsUseCase.run(deals);
+
+      await Promise.all([createOrders, saveManyDeals]);
+
+      return HttpResponse.ok({ data: deals });
+    } catch (error) {
+      console.error(error);
+      return HttpResponse.serverError();
+    }
+  }
+}
