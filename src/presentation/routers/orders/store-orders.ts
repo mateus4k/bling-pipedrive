38129cedc5f57fb52b0900f71d20cbb3ada4f6ac
Mateus <matheus.sampaio@bee.com.br>
2020-09-16 22:10:03 -0300

    fix(orders): typo

--- src/presentation/routers/orders/store-orders.ts
+++ src/presentation/routers/orders/store-orders.ts
@@ -4,20 +4,20 @@ import { HttpResponse, ResponseInterface } from '../../helpers';
 
 export class StoreOrdersRouter implements RouterInterface {
   constructor(
-    private getDealsPipedriveCase: UseCaseInterface,
-    private createOrdersBlingCase: UseCaseInterface,
+    private getDealsPipedriveUseCase: UseCaseInterface,
+    private createOrdersBlingUseCase: UseCaseInterface,
     private saveManyDealsUseCase: UseCaseInterface,
   ) {}
 
   async route(): Promise<ResponseInterface> {
     try {
-      const deals = await this.getDealsPipedriveCase.run();
+      const deals = await this.getDealsPipedriveUseCase.run();
 
       if (!Array.isArray(deals)) {
         return HttpResponse.notFoundError();
       }
 
-      const createOrders = () => this.createOrdersBlingCase.run(deals);
+      const createOrders = () => this.createOrdersBlingUseCase.run(deals);
       const saveManyDeals = () => this.saveManyDealsUseCase.run(deals);
 
       await Promise.all([createOrders, saveManyDeals]);
