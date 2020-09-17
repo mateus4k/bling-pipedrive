3d2490baa2ad389bc13e64378dd45a7c2f7f1e8e
Mateus <matheus.sampaio@bee.com.br>
2020-09-16 22:49:39 -0300

    test(deals): coverage methods

--- src/infra/repositories/__tests__/deals-repository.spec.ts
+++ src/infra/repositories/__tests__/deals-repository.spec.ts
@@ -17,11 +17,42 @@ describe('DealsRepository', () => {
     await MongoHelper.disconnect();
   });
 
-  it('returns 200', async () => {
+  it('all', async () => {
+    const repository = new DealsRepository();
+
+    const response = await repository.all();
+
+    expect(response).toEqual([]);
+  });
+
+  it('find', async () => {
+    const repository = new DealsRepository();
+
+    const objectToBeInserted = { message: 'hi' };
+
+    await repository.insert(objectToBeInserted);
+    const response = await repository.find(objectToBeInserted);
+
+    expect(response).toEqual(objectToBeInserted);
+  });
+
+  it('insert', async () => {
     const repository = new DealsRepository();
 
     const response = await repository.insert({ message: 'hi' });
 
     expect(response.ok).toBeTruthy();
+  });
+
+  it('insertMany', async () => {
+    const repository = new DealsRepository();
+
+    const arrayToBeInserted = [{ data: 1 }, { data: 2 }, { data: 3 }];
+
+    const response = await repository.insertMany(arrayToBeInserted);
+    const all = await repository.all();
+
+    expect(response.ok).toBeTruthy();
+    expect(all).toEqual(arrayToBeInserted);
   });
 });
